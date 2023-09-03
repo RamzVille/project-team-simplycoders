876f23e2ce0f6858c3d842f539c0e8242a8f0253
vinn-19 <vher_vier@yahoo.com>
2023-09-03 23:21:48 +0800

    update1

---
+++ src/chocoisloved-swiper.js
@@ -0,0 +1,31 @@
+var swiper = new Swiper('.mySwiper', {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  slidesPerGroup: 3,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  //   centeredSlides: true,
+  lazyLoading: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 28,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    375: {
+      slidesPerView: 1,
+      spaceBetween: 1,
+    },
+  },
+});

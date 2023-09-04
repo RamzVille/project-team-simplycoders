db7b5f0100cd0d3a1096a4cf369f1712adfbc529
Ramz Ville <136719696+RamzVille@users.noreply.github.com>
2023-09-04 16:56:50 +0800

    Update buy-order-modal.js

--- src/buy-order-modal.js
+++ src/buy-order-modal.js
@@ -1,15 +1,15 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+    openBuyBtn: document.querySelector("[data-buy-open]"),
+    closeBuyBtn: document.querySelector("[data-buy-close]"),
+    buy: document.querySelector("[data-buy]"),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openBuyBtn.addEventListener("click", toggleModal);
+  refs.closeBuyBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+    refs.buy.classList.toggle("is-hidden");
   }
 
   

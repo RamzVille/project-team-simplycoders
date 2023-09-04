a85ec80c36b8d3ad8e833321e9ee0f48b56438f7
vinn-19 <vher_vier@yahoo.com>
2023-09-05 00:13:41 +0800

    commit3

--- src/chocoisloved-modal.js
+++ src/chocoisloved-modal.js
@@ -1,14 +1,14 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
+    openModalBtn: document.querySelector('[data-chocoloved-open]'),
+    closeModalBtn: document.querySelector('[data-chocoloved-close]'),
+    modal: document.querySelector('[data-chocoloved]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
+    refs.modal.classList.toggle('chocoloved-is-hidden');
   }
 })();

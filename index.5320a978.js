d6d9ac8040c78821c4a106895b16b55f731ad7a8
RamzVille <136719696+RamzVille@users.noreply.github.com>
2023-09-04 08:57:29 +0000

    Deploying to gh-pages from @ RamzVille/project-team-simplycoders@dc502cc067089980bc6003815d7976ba24f78fe3 🚀

---
+++ index.5320a978.js
@@ -0,0 +1,2 @@
+!function(){var e={openBuyBtn:document.querySelector("[data-buy-open]"),closeBuyBtn:document.querySelector("[data-buy-close]"),buy:document.querySelector("[data-buy]")};function t(){e.buy.classList.toggle("is-hidden")}e.openBuyBtn.addEventListener("click",t),e.closeBuyBtn.addEventListener("click",t)}();
+//# sourceMappingURL=index.5320a978.js.map

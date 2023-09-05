146eeb7ddc1c231d42b5b4f774b9d46b810cc7c0
Ramz Ville <136719696+RamzVille@users.noreply.github.com>
2023-09-05 21:05:30 +0800

    Update chocoisloved-modal.js

--- src/chocoisloved-modal.js
+++ src/chocoisloved-modal.js
@@ -1,33 +1,3 @@
-// (() => {
-//   const refs = {
-//     openModalBtn: document.querySelector('[data-chocoloved-open]'),
-//     closeModalBtn: document.querySelector('[data-chocoloved-close]'),
-//     modal: document.querySelector('[data-chocoloved]'),
-//   };
-
-//   refs.openModalBtn.addEventListener('click', toggleModal);
-//   refs.closeModalBtn.addEventListener('click', toggleModal);
-
-//   function toggleModal() {
-//     refs.modal.classList.toggle('chocoloved-is-hidden');
-//   }
-// })();
-
-// (() => {
-//   const refs = {
-//     openModalBtn: document.querySelector('[data-chocoloved-open]'),
-//     closeModalBtn: document.querySelector('[data-chocoloved-close]'),
-//     modal: document.querySelector('[data-chocoloved]'),
-//   };
-
-//   refs.openModalBtn.addEventListener('click', toggleModal);
-//   refs.closeModalBtn.addEventListener('click', toggleModal);
-
-//   function toggleModal() {
-//     refs.modal.classList.toggle('chocoloved-backdrop'); // Use refs.modal, not refs.chocoloved
-//   }
-// })();
-
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[data-chocoloved-open]'),

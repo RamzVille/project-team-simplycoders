bead49b0ed442f6d00156f473343f068374c06d4
Ramz Ville <136719696+RamzVille@users.noreply.github.com>
2023-09-05 21:04:42 +0800

    Add files via upload

--- src/chocoisloved-modal.js
+++ src/chocoisloved-modal.js
@@ -1,14 +1,44 @@
+// (() => {
+//   const refs = {
+//     openModalBtn: document.querySelector('[data-chocoloved-open]'),
+//     closeModalBtn: document.querySelector('[data-chocoloved-close]'),
+//     modal: document.querySelector('[data-chocoloved]'),
+//   };
+
+//   refs.openModalBtn.addEventListener('click', toggleModal);
+//   refs.closeModalBtn.addEventListener('click', toggleModal);
+
+//   function toggleModal() {
+//     refs.modal.classList.toggle('chocoloved-is-hidden');
+//   }
+// })();
+
+// (() => {
+//   const refs = {
+//     openModalBtn: document.querySelector('[data-chocoloved-open]'),
+//     closeModalBtn: document.querySelector('[data-chocoloved-close]'),
+//     modal: document.querySelector('[data-chocoloved]'),
+//   };
+
+//   refs.openModalBtn.addEventListener('click', toggleModal);
+//   refs.closeModalBtn.addEventListener('click', toggleModal);
+
+//   function toggleModal() {
+//     refs.modal.classList.toggle('chocoloved-backdrop'); // Use refs.modal, not refs.chocoloved
+//   }
+// })();
+
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
+    openModalBtn: document.querySelector('[data-chocoloved-open]'),
+    closeModalBtn: document.querySelector('[data-chocoloved-close]'),
+    modal: document.querySelector('[data-chocoloved]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
+    refs.modal.classList.toggle('chocoloved-is-hidden'); // Use refs.modal and the correct class name
   }
 })();

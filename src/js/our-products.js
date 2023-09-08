4712df416be744da5d0d02d71922f0edce0a73fd
Ramz Ville <ramzville22@gmail.com>
2023-09-09 00:43:34 +0800

    final-3

--- src/js/our-products.js
+++ src/js/our-products.js
@@ -1,31 +1,40 @@
 var swiper = new Swiper('.mySwiperProducts', {
   slidesPerView: 1,
-  spaceBetween: 0,
-  slidesPerGroup: 1,
-  // loop: true,
-  loopFillGroupWithBlank: true,
-    // centeredSlides: true,
-  lazyLoading: true,
+  slidesPerView: 1,
+  spaceBetween: 8,
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
   },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
   breakpoints: {
-    1200: {
-      slidesPerView: 1,
-      spaceBetween: 18,
-    },
     768: {
-      slidesPerView: 2,
-      spaceBetween: 20,
+      slidesPerView:3,
+      spaceBetween: 8,
     },
-    375: {
-      slidesPerView: 1,
+    1200: {
+      slidesPerView: 4,
       spaceBetween: 18,
     },
   },
 });
+
+  var swiper = new Swiper(".reviewSwiper", {
+      slidesPerView: 1,
+     spaceBetween: 8,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    breakpoints: {
+          
+
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 16,
+    },
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 28,
+    },
+  },
+    });
